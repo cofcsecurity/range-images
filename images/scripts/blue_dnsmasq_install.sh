e911944f41a0cd6660af3776e6757efd517e11f2
lukem1 <37024616+lukem1@users.noreply.github.com>
2021-11-13 22:48:47 -0500

    Updated dnsmasq config

--- images/scripts/blue_dnsmasq_install.sh
+++ images/scripts/blue_dnsmasq_install.sh
@@ -13,31 +13,20 @@ CONFIG="
 no-resolv
 server=1.1.1.1
 server=8.8.8.8
+listen-address=127.0.0.1,10.0.10.5
+
+# Range Hosts
+address=/dns.blue/10.0.10.5
+address=/mongo.blue/10.0.10.10
+address=/chat.blue/10.0.10.15
+address=/work.blue/10.0.10.20
+
+address=/ad.blue/10.0.10.50
+address=/web.blue/10.0.10.55
 "
 
 echo "Creating config file..."
 echo "$CONFIG" | sudo tee /etc/dnsmasq.conf > /dev/null
-
-# Range host configuration
-HOSTS="
-127.0.0.1	localhost
-::1	localhost ip6-localhost ip6-loopback
-fe00::0	ip6-localnet
-ff00::0	ip6-mcastprefix
-ff02::1	ip6-allnodes
-ff02::2	ip6-allrouters
-
-10.0.10.5 dns.blue
-10.0.10.10 mongo.blue
-10.0.10.15 chat.blue
-10.0.10.20 work.blue
-
-10.0.10.50 ad.blue
-10.0.10.55 web.blue
-"
-
-echo "Creating hosts file..."
-echo "$HOSTS" | sudo tee /etc/hosts > /dev/null
 
 echo "Enabling service..."
 sudo systemctl enable dnsmasq
