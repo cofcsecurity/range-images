59cee9153704dcbeee83e427535ad1a00ddee8f6
Patrick McCabe <mccabepj@g.cofc.edu>
2022-01-28 20:55:12 -0600

    Syntax update

--- images/scripts/database.sql
+++ images/scripts/database.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+CREATE DATABASE wordpress;
 GRANT ALL ON wordpress.* TO 'wordpress'@'localhost' IDENTIFIED BY 'password';
 FLUSH PRIVILEGES;
 EXIT;
