cc0bb16db0f93619ac9a6886cbf6f7fb9a476e48
lukem1 <37024616+lukem1@users.noreply.github.com>
2022-02-24 13:40:19 -0500

    Update httpd config

--- images/scripts/blue_httpd_install.sh
+++ images/scripts/blue_httpd_install.sh
@@ -14,15 +14,86 @@ echo "apt update and install..."
 sudo apt update
 sudo apt install -y apache2
 
-CONFIG="ScriptAlias /cgi-bin/ /var/cgi-bin/
+CONFIG="DefaultRuntimeDir \${APACHE_RUN_DIR}
+
+PidFile \${APACHE_PID_FILE}
+
+Timeout 300
+
+KeepAlive On
+
+MaxKeepAliveRequests 100
+
+KeepAliveTimeout 5
+
+
+User \${APACHE_RUN_USER}
+Group \${APACHE_RUN_GROUP}
+
+HostnameLookups Off
+
+ErrorLog \${APACHE_LOG_DIR}/error.log
+
+LogLevel warn
+
+IncludeOptional mods-enabled/*.load
+IncludeOptional mods-enabled/*.conf
+
+Include ports.conf
+
+
+<Directory />
+	Options FollowSymLinks
+	AllowOverride None
+	Require all denied
+</Directory>
+
+<Directory /usr/share>
+	AllowOverride None
+	Require all granted
+</Directory>
+
+<Directory /var/www/>
+	Options Indexes FollowSymLinks
+	AllowOverride None
+	Require all granted
+</Directory>
+
+ScriptAlias /cgi-bin/ /var/cgi-bin/
 <Directory \"/var/cgi-bin\">
    AllowOverride None
    Options +ExecCGI -MultiViews +SymLinksIfOwnerMatch
    Require all granted
-</Directory>"
+</Directory>
+<Directory /srv/>
+	Options Indexes FollowSymLinks
+	AllowOverride None
+	Require all granted
+</Directory>
 
-echo "Appending to config file..."
-echo "$CONFIG" | sudo tee -a /etc/apache2/apache2.conf > /dev/null
+
+
+
+AccessFileName .htaccess
+
+<FilesMatch \"^\.ht\">
+	Require all denied
+</FilesMatch>
+
+
+LogFormat \"%v:%p %h %l %u %t \\\"%r\\\" %>s %O \\\"%{Referer}i\\\" \\\"%{User-Agent}i\\\"\" vhost_combined
+LogFormat \"%h %l %u %t \\\"%r\\\" %>s %O \\\"%{Referer}i\\\" \\\"%{User-Agent}i\\\"\" combined
+LogFormat \"%h %l %u %t \\\"%r\\\" %>s %O\" common
+LogFormat \"%{Referer}i -> %U\" referer
+LogFormat \"%{User-agent}i\" agent
+
+
+IncludeOptional conf-enabled/*.conf
+
+IncludeOptional sites-enabled/*.conf"
+
+echo "Creating config file..."
+echo "$CONFIG" | sudo tee /etc/apache2/apache2.conf > /dev/null
 
 echo "Enable CGI scripts..."
 sudo a2enmod cgi.load
